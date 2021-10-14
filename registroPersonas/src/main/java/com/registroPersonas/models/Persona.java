8ab0115cfe805362e76adb049b754ddd8538b7b6
Fernando <gatoguevara2@hotmail.com>
2021-10-13 21:43:29 -0500

    Creacion de Entidades

---
+++ registroPersonas/src/main/java/com/registroPersonas/models/Persona.java
@@ -0,0 +1,114 @@
+package com.registroPersonas.models;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the personas database table.
+ * 
+ */
+@Entity
+@Table(name="personas")
+@NamedQuery(name="Persona.findAll", query="SELECT p FROM Persona p")
+public class Persona implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String cedula;
+
+	private String apellidos;
+
+	private String canton;
+
+	private String correo;
+
+	private String direccion;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="fecha_nacimiento")
+	private Date fechaNacimiento;
+
+	private String nombres;
+
+	private String provincia;
+
+	private String telefono;
+
+	public Persona() {
+	}
+
+	public String getCedula() {
+		return this.cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getApellidos() {
+		return this.apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getCanton() {
+		return this.canton;
+	}
+
+	public void setCanton(String canton) {
+		this.canton = canton;
+	}
+
+	public String getCorreo() {
+		return this.correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getDireccion() {
+		return this.direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public Date getFechaNacimiento() {
+		return this.fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getNombres() {
+		return this.nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getProvincia() {
+		return this.provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getTelefono() {
+		return this.telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
